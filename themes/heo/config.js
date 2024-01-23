171ee80f7ff8c6b58c3a815335db0e49b4e9eebf
qwx <qiuwx97@outlook.com>
2024-01-23 10:24:29 +0800

    #

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://www.civ6barbaricup.fun/',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '比赛奖金池', url: '/example-3' },
-  HEO_HERO_CATEGORY_2: { title: '比赛赞助方', url: 'example-3' },
-  HEO_HERO_CATEGORY_3: { title: '比赛主办方', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '比赛奖金池', url: '/article/prize' },
+  HEO_HERO_CATEGORY_2: { title: '比赛赞助方', url: '/article/support' },
+  HEO_HERO_CATEGORY_3: { title: '比赛主办方', url: '/article/owner' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '战队',
