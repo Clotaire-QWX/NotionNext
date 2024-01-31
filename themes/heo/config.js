5b17e5a3c274a5f641f140be8a50dcbeb29d23ed
qwx <qiuwx97@outlook.com>
2024-01-31 11:53:42 +0800

    #

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎关注巴巴里杯系列赛事', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎关注巴巴里杯系列赛事', url: 'https://www.civ6barbaricup.fun/' },
+    { title: '目前正处于S2赛季', url: 'https://www.civ6barbaricup.fun/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -28,7 +28,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '比赛主办方', url: '/article/owner' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '战队',
+  HEO_HERO_RECOMMEND_POST_TAG: '队伍信息',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
